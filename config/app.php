dacee3a59e7fc7d5f66688abc8b110077049366a
nishant <nishanttrivedi2010@gmail.com>
2021-06-19 03:42:12 +0530

    Initial commit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://http://forum.nishanttrivedi98.in/'),
 
     'asset_url' => env('ASSET_URL', null),
 
